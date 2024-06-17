8b5a3a8d0c8ad0659fc5c7f1ff268917b5aee31c
Nicholas Bauer <nicholasbauer@outlook.com>
2024-06-16 22:27:14 -0400

    Add project files.

---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace MauiApp5
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public override void OnCreate()
+        {
+#if DEBUG
+            // Warn if the app is engaged in behavior that could result in
+            // a nonresponsive period.
+            Android.OS.StrictMode.SetThreadPolicy(new Android.OS.StrictMode.ThreadPolicy.Builder()
+                .DetectAll()
+                .PenaltyLog()
+                .Build());
+#endif
+            base.OnCreate();
+        }
+    }
+}
